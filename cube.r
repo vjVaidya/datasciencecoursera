16ee229e7158e5b62558f0e3d925e8e1c266c529
vjVaidya <vyasrajvaidya@gmail.com>
2017-07-04 16:18:40 +0200

    finished the cacheMatrix assignment

---
+++ cube.r
@@ -0,0 +1,10 @@
+cube <- function(x, n) {
+  x^3
+  }
+
+cube(3)
+
+x <- 1:10
+if(x > 5) {
+  x <- 0
+}
---
+++ r-funcs.R
@@ -0,0 +1,24 @@
+add2 <- function(x,y) {
+  x+y
+}
+
+above10 <- function(x){
+  use <- x > 10
+  x[use]
+}
+
+above <- function(x,n){
+  use <- x>n
+  x[use]
+}
+
+colMean <- function(y, removeNA = TRUE){
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for(i in 1:nc){
+    means[i] <- mean(y[, i], na.rm = removeNA)
+    
+  }
+  means
+}
+
